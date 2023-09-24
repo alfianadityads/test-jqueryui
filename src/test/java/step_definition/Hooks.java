fb7315796299470fee242f0a634a0ae567b408e3
alfianadityads <alfianadityadwi@gmail.com>
2023-09-24 19:25:41 +0700

    feat: add step for open browser, open link and close browser

---
+++ src/test/java/step_definition/Hooks.java
@@ -0,0 +1,36 @@
+package step_definition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+    public static WebDriver driver;
+
+    @Before
+    public void openBrowser() {
+        // fungsi untuk clear chace chrome driver
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+
+        // fungsi untuk setup chrome agar bisa terbuka
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+
+        // inisiasi library selenium
+        driver = new ChromeDriver(co);
+
+        String appUrl = "https://jqueryui.com/";
+        driver.get(appUrl); // fungsi membuka link html
+        driver.manage().window().maximize(); // fungsi untuk maximize browser
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
